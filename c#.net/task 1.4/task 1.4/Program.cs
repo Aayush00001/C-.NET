da21aec344547c3a29a5c982ff1104ea89ebaaba
Aayush Radhnapara <aayushradhanpara0001@gmail.com>
2024-05-05 15:21:11 +0530

    progrmas

---
+++ c#.net/task 1.4/task 1.4/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace task_1._4
+{
+    //Write c# sharp program to create menu driven application as below
+    //Menu: 1. Palindrome number.
+    //2. Armstrong number.
+    //3. Write a program that converts Centigrade to Fahrenheit.
+    //4. Write a program that converts kilometers per hour to miles per hour.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: radhanpara aayush.");
+            Console.WriteLine("Enrollment No: 210801258.");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("0. Exit!");
+            Console.WriteLine("1. Palindrome Number.");
+            Console.WriteLine("2. Armstong Number.");
+            Console.WriteLine("3. Convert Centigrade to Fahrenheit.");
+            Console.WriteLine("4. Convert Km/hr to M/hr.");
+            Console.WriteLine("Enter your Choice:");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 0:
+                    break;
+                case 1:
+                    {
+                        int n, r, sum = 0, temp;
+                        Console.WriteLine("Enter The Number:");
+                        n = int.Parse(Console.ReadLine());
+                        temp = n;
+                        while (n > 0)
+                        {
+                            r = n % 10;
+                            sum = (sum * 10) + r;
+                            n = n / 10;
+                        }
+                        if (temp == sum)
+                            Console.WriteLine("The Number is Palindrome.");
+                        else
+                            Console.WriteLine("The Number is not Palindrome.");
+                        break;
+                    }
+                case 2:
+                    {
+                        int n, r, sum = 0, temp;
+                        Console.Write("Enter the Number= ");
+                        n = Convert.ToInt32(Console.ReadLine());
+                        temp = n;
+                        while (n > 0)
+                        {
+                            r = n % 10;
+                            sum = sum + (r * r * r);
+                            n = n / 10;
+                        }
+                        if (temp == sum)
+                            Console.Write("The Number is Armstrong Number.");
+                        else
+                            Console.Write("The Number is Not Armstrong Number.");
+                        break;
+                    }
+                case 3:
+                    {
+                        double fahrenheit;
+                        Console.WriteLine("Enter Centigrade or Celsius");
+                        double celsius = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Celsius: " + celsius);
+                        fahrenheit = (celsius * 9) / 5 + 32;
+                        Console.WriteLine("Fahrenheit: " + fahrenheit);
+                        Console.ReadLine();
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Enter kilometers:");
+                        double kilometers = Convert.ToDouble(Console.ReadLine());
+                        double miles = kilometers / 1.6;
+                        Console.WriteLine(miles + " Miles");
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ c#.net/task 1.5/task 1.5/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace task_1._5
+{
+        //Write c# sharp program to create menu driven application as below
+        //Menu:
+        //1. Write a program that takes hours and minutes as input, and calculates the total number of minutes.
+        //2.Write a program in that takes minutes as input, and display the total number of hours and minutes. 
+        //3.Write a program that accepts three integers from the user and finds the second largest number among
+        //them.
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Name: radhanpara aayush.");
+                Console.WriteLine("Enrollment No: 210801258.");
+                Console.WriteLine(DateTime.Now);
+                Console.WriteLine("0. Exit!");
+                Console.WriteLine("1. Input Hours and get Minutes.");
+                Console.WriteLine("2. Input Minutes andget Hours and Minutes.");
+                Console.WriteLine("3. For Second Largest Number from the Input.");
+                int ch = Convert.ToInt32(Console.ReadLine());
+                switch (ch)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        {
+                            Console.WriteLine("Enter hours:");
+                            int hours = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter minutes:");
+                            int minutes = Convert.ToInt32(Console.ReadLine());
+                            int totalMinutes = (hours * 60) + minutes;
+                            Console.WriteLine("Total number of minutes: " + totalMinutes);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Enter minutes:");
+                            int minutes = Convert.ToInt32(Console.ReadLine());
+                            int hours = minutes / 60;
+                            int remainingMinutes = minutes % 60;
+                            Console.WriteLine("Total hours: " + hours);
+                            Console.WriteLine("Total minutes: " + remainingMinutes);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Enter three integers:");
+                            int num1 = int.Parse(Console.ReadLine());
+                            int num2 = int.Parse(Console.ReadLine());
+                            int num3 = int.Parse(Console.ReadLine());
+                            int secondLargest;
+                            if (num1 >= num2 && num1 >= num3)
+                            {
+                                if (num2 >= num3)
+                                {
+                                    secondLargest = num2;
+                                }
+                                else
+                                {
+                                    secondLargest = num3;
+                                }
+                            }
+                            else if (num2 >= num1 && num2 >= num3)
+                            {
+                                if (num1 >= num3)
+                                {
+                                    secondLargest = num1;
+                                }
+                                else
+                                {
+                                    secondLargest = num3;
+                                }
+                            }
+                            else
+                            {
+                                if (num1 >= num2)
+                                {
+                                    secondLargest = num1;
+                                }
+                                else
+                                {
+                                    secondLargest = num2;
+                                }
+                            }
+                            Console.WriteLine("The second largest number is: " +
+                           secondLargest);
+                            break;
+                        }
+                }
+            }
+        }
+    }
+
