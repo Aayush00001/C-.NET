26bfb475d1badf74f1eafea0af845c93ad8721e6
Aayush Radhnapara <aayushradhanpara0001@gmail.com>
2024-05-05 15:48:13 +0530

    p

---
+++ c#.net/task3_10/task3_10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace task3_10
+{
+   
+        // Define the interface
+        interface Test
+        {
+            int Square(int num);
+        }
+
+        // Implement the interface in Arithmetic class
+        class Arithmetic : Test
+        {
+            public int Square(int num)
+            {
+                return num * num;
+            }
+        }
+
+        // Use the object of Arithmetic class in ToTestInt class
+        class ToTestInt
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("name:radhanpara aayush");
+                Console.WriteLine("enroll:210801258");
+                Console.WriteLine(DateTime.Now);
+
+                Arithmetic arithmetic = new Arithmetic();
+
+                Console.Write("Enter a number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                int result = arithmetic.Square(number);
+                Console.WriteLine("Square of " + number + " is: " + result);
+            }
+        }
+    }
+
+
